44103baa55dd615cae8d01e422ce47de76f8b8ae
Alex <angelic97.andersson@gmail.com>
2021-06-10 01:00:08 +0200

    changed some css

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
-import React from 'react';
+import React from "react";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-    <div className ="page-wrapper">
+      <div className="page-wrapper">
+        <section className="menu">
+          <img></img>
+          <div className="menu-items">
+            <li>Projects</li>
+            <li>Skills</li>
+            <li>Experiences</li>
+            <li>About Me</li>
+          </div>
+          <div className=""></div>
+        </section>
 
-
-      <section className="menu">
-        <img></img>
-        <div className="menu-items">
-          <li>testing</li>
-          <li></li>
-          <li></li>
-        </div>
-        <div className=""></div>
-      </section>
-
-
-      <section className="content">
-
-        <div className ="test">
-
-        </div>
-
-
-      </section>
-
+        <section className="content">
+          <div className="intro-part">
+            <h3>Hi! My name is Angelic Andersson</h3>
+            <br></br>
+            <h3>
+              I am a junior frontend developer with big interest in UI & UX
+            </h3>
+          </div>
+          <section className="projects">Projects</section>
+          <section className="skills">Skills</section>
+          <section className="experiences">Experiences</section>
+        </section>
       </div>
     </div>
   );
