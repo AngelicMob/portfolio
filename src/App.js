9fa7f6007603c7ef2ae29fcd3d8abb0c33fc1596
Alex <angelic97.andersson@gmail.com>
2021-06-11 01:48:21 +0200

    changed more css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { AiOutlineGithub  } from 'react-icons/ai';
+import {FaLinkedin} from 'react-icons/fa'
 import "./App.css";
 
 function App() {
@@ -7,23 +9,33 @@ function App() {
       <div className="page-wrapper">
         <section className="content">
           <div className="intro-part">
-            <p>Hi! My name is Angelic Andersson</p>
+            <p>Hi! My name is <strong>Angelic Andersson</strong></p>
             <br></br>
             <p>I am a junior frontend developer with big interest in UI & UX</p>
           </div>
-          <section className="projects">Projects</section>
-          <section className="skills">Skills</section>
-          <section className="experiences">Experiences</section>
+          <section className="projects"></section>
+          <section className="skills"></section>
+          <section className="experiences"></section>
         </section>
         <section className="menu">
-          <img></img>
+          <img className="profile"></img>
           <div className="menu-items">
             <li>Projects</li>
             <li>Skills</li>
             <li>Experiences</li>
             <li>About Me</li>
           </div>
-          <div className=""></div>
+          <div className="contacts">
+            <p>Social Medias</p>
+          <div className ="media-icons">
+          <a href="https://www.linkedin.com/in/angelic-andersson-20a522192/">
+          <FaLinkedin/>
+          </a>
+          <a href="https://github.com/AngelicMob?tab=repositories">
+          <AiOutlineGithub/>
+          </a>
+            </div>
+          </div>
         </section>
       </div>
     </div>
