2d7ace46967a845d95710c9cf69bc1f365c2bce5
Alex <angelic97.andersson@gmail.com>
2021-06-18 04:45:00 +0200

    social media buttons fixing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from "react";
-import { AiOutlineGithub  } from 'react-icons/ai';
-import {FaLinkedin} from 'react-icons/fa'
+import { AiOutlineGithub } from "react-icons/ai";
+import { FaLinkedin } from "react-icons/fa";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <div className="page-wrapper">
-
         <section className="content">
           <div className="intro-part">
-            <p>Hi! My name is <strong>Angelic Andersson</strong></p>
+            <p>
+              Hi! My name is <strong>Angelic Andersson</strong>
+            </p>
             <br></br>
             <p>I am a junior frontend developer with big interest in UI & UX</p>
           </div>
@@ -29,13 +30,19 @@ function App() {
           </div>
           <div className="contacts">
             <p>Social Medias</p>
-          <div className ="media-icons">
-          <a href="https://www.linkedin.com/in/angelic-andersson-20a522192/">
-          <FaLinkedin/>
-          </a>
-          <a href="https://github.com/AngelicMob?tab=repositories">
-          <AiOutlineGithub/>
-          </a>
+            <div className="media-icons">
+              <section className="media-linkedin">
+                <a href="https://www.linkedin.com/in/angelic-andersson-20a522192/">
+                  <FaLinkedin />
+                  <p className="media-text">LinkedIn</p>
+                </a>
+              </section>
+              <section className="media-github">
+                <a href="https://github.com/AngelicMob?tab=repositories">
+                  <AiOutlineGithub />
+                  <p className="media-text">Github</p>
+                </a>
+              </section>
             </div>
           </div>
         </section>
